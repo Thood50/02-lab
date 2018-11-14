b33ad3da896822b9fe6a0733fe384dc2996c27df
tyler <tyler.r.hood@gmail.com>
2018-11-14 09:38:39 -0800

    Displayed Images

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 02-lab
+
+Number and Name of feature: Display Images;
+Estimate of time needed to complete: 1 hour;
+Start time: 9:05;
+Finsih time: 9:35;
+Actual time needed to complte: 30 minutes;
--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 'use strict'
 
-let grabData = function() {
-    $.getJSON('data.json', data => {
-        data.forEach(object => {
-            new Horns(object);
-        })
-    }).then( renderHorns );
+function Horns(obj) {
+    this.image = obj.image_url;
+    this.title = obj.title;
+    this.description = obj.description;
+    this.keyword = obj.keyword;
+    this.horns = obj.horns;
 }
+
+Horns.prototype.render = function() {
+    $('main').append('<div class="horny"></div>');
+    let objHorny = $('div[class="horny"]');
+    let objHtml = $('#photo-template').html();
+    objHorny.html(objHtml);
+
+    objHorny.find('img').attr('src', this.image);
+    objHorny.find('h2').text(this.title);
+    objHorny.find('p').text(this.description);
+    objHorny.find('img').attr('alt', this.keyword);
+    objHorny.removeClass('clone');
+    objHorny.attr('class', this.keyword);
+}
+
+Horns.Array = [];
+
+Horns.readJson = () => {
+    $.get('data.JSON', 'json') 
+    .then(data => {
+        data.forEach(obj => {
+            Horns.Array.push(new Horns(obj));
+        })
+    })
+    .then(Horns.loadHorns)
+}
+
+Horns.loadHorns = () => {
+    Horns.Array.forEach(obj => obj.render());
+    console.log('ran loadHorns');
+}
+
+$(() => Horns.readJson());
